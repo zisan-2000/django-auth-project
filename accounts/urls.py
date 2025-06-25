c5e790ad74a0bda0f99686f36fb35f1065dcbdeb
BoedZisan <stack1@birdsofeden.me>
2025-06-25 22:52:19 +0600

    🔐 Replace session-based login with JWT authentication using SimpleJWT

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import RegisterView, LoginView, LogoutView, UserViewSet,VerifyEmailView,PasswordResetRequestView,PasswordResetConfirmView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView
+)
+
+
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='user')
@@ -14,7 +22,9 @@ urlpatterns = [
     path('', include(router.urls)),  # ✅ Add this
     path('verify-email/<uidb64>/<token>/', VerifyEmailView.as_view(), name='verify-email'),
     path('reset-password/', PasswordResetRequestView.as_view(), name='reset-password-request'),
-path('reset-password-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='reset-password-confirm'),
-
-
+    path('reset-password-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='reset-password-confirm'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  
+    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),  
 ]
--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+from datetime import timedelta
+
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,21 +56,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        # চাইলে শুধু TokenAuthentication রাখতে পারেন:
-        # 'rest_framework.authentication.TokenAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # আপনি চাইলে এখানে IsAuthenticated ও ব্যবহার করতে পারেন
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
